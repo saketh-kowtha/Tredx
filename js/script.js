fe76fe486d9a7284590c139de1da6d4f24e77748
SAKETH KOWTHA <sakethkowtha@SAKETHs-MacBook-Pro-2.local>
2018-07-12 10:32:24 +0530

    Front-End

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 let home = document.getElementById('explore')
 home.addEventListener('click',function(){
-    scroll_to_div('main')
+    toastr.warning('My name is Inigo Montoya. You killed my father, prepare to die!')
+
+    // scroll_to_div('main')
+
 })
 
 function scroll_to_div(div_id)
@@ -10,4 +13,26 @@ function scroll_to_div(div_id)
   scrollTop: $("#"+div_id).offset().top
  },
  'slow');
+}
+
+let send = document.getElementById('send')
+send.addEventListener('click',function(){
+    let user = document.getElementById('name')
+    let email = document.getElementById('email')
+    let cnt = document.getElementById('cnt')
+
+})
+
+function getContent(){
+    console.log("Abc")
+    let xhttp = new XMLHttpRequest()
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+           // Typical action to be performed when the document is ready:
+           document.getElementById("main").innerHTML = xhttp.responseText;
+           console.log(xhttp.responseText)
+        }
+    };
+    xhttp.open("GET", "js/update.txt", true);
+    xhttp.send();
 }
