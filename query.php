7c4e1369ebe25dcdb63c2cae7976fa2ad3ad2712
saketh <saketh.kowtha@gmail.com>
2018-07-12 13:26:10 +0000

    Dev1

--- query.php
+++ query.php
@@ -3,7 +3,7 @@
     $email = $_POST['email'];
     $cnt = $_POST['cnt'];
     $myfile = fopen("js/query.txt", "a") or die("Unable to open file!".$error_message);
-    $ret = fwrite($myfile, $name."------------------------------------".$email."------------------------------------".$cnt."-----------------------------------------------------------------------------------------------------------------------------------------------------------");
+    $ret = fwrite($myfile, "Name => ".$name."\nEMail => ".$email."\nContent => ".$cnt."\n\n\n");
     fclose($myfile);
     echo  "Success"
 ?>
